0f24f53aea3a927ae99653add858aa02a9ec92ef
devin-web <devin@alumni.sfu.ca>
2016-09-28 20:16:45 +0000

    Test of Hello World

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
       password : process.env.DB_PASS,
       database : process.env.DB_NAME,
       port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      ssl      : false
     },
     migrations: {
       directory: './db/migrations',
