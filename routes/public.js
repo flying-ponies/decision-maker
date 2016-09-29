21dce83e4fdf24d8f85985cff7713943b78fd72a
John Chow <JChow417@gmail.com>
2016-09-29 02:16:48 +0000

    completed routes

--- routes/public.js
+++ routes/public.js
@@ -7,7 +7,6 @@ module.exports = (knex) => {
 
   router.get('/polls/:key', (req, res) => {
     let publicPollKey = req.params.key;
-    console.log("HEY");
     knex
       .select("public_key")
       .from("polls")
@@ -15,16 +14,31 @@ module.exports = (knex) => {
       .then((results) => {
         if (results.length) {
           //GETS INFO FROM DB
-          knex
-            .select("pollers.email","polls.question","polls.is_open","choices.title","choices.description")
-            .from("polls")
-            .innerJoin("pollers","poll.poller_id","polls.id")
-            .innerJoin("choices","polls.id","choices.poll_id")
-            .where("public_key", publicPollKey)
-            .then((results) => {
-              console.log(results);
-            });
+          // knex.multiQuery([
+          //   knex
+          //     .select("pollers.email","polls.question","polls.is_open")
+          //     .from("polls")
+          //     .innerJoin("pollers","polls.poller_id","pollers.id")
+          //     .innerJoin("choices","polls.id","choices.poll_id")
+          //     .where("public_key", publicPollKey),
+          //   knex
+          //     .select("choices.title","choices.description", "points")
+          //     .from("polls")
+          //     .innerJoin("pollers","polls.poller_id","pollers.id")
+          //     .innerJoin("choices","polls.id","choices.poll_id")
+          //     .where("public_key", publicPollKey),
+          // ]).spread((resultA, resultB) => {
+          //   console.log(resultA);
+          //   console.log(resultB);
+          // });
 
+                // let templateVars = {
+                //   email: results[0].email,
+                //   title: results[0].title,
+                //   longURL: longURL,
+                //   "hostName": hostName
+                // };
+                // console.log(templateVars);
 
           //PUT INFO IN TO OJBECT
           //RENDER PAGE USING EJS WITH OBJECT
