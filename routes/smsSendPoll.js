52ebf2471854a261363620bbaa8bb74ed3859ab8
devin-web <devin@alumni.sfu.ca>
2016-09-30 03:37:49 +0000

    Bug fixes for select query due to req.body over req.params contain salient data

--- routes/smsSendPoll.js
+++ routes/smsSendPoll.js
@@ -4,21 +4,23 @@ const express = require('express');
 const router  = express.Router();
 const smsDialler = require('./util/smsDialler');
 
+
 module.exports = (knex) => {
   router.post( '/sms/sendpoll', (req, res) => {
-    const phoneNumber = req.params["phone-number"];
-    const privateKey = req.params.private_key;
+    const phoneNumber = req.body["phone-number"];
+    const privateKey = req.body.private_key;
+    console.log( privateKey );
     var smsPoll = "";
     knex
-      .select("question, title")
-      .from("polls")
-      .join("choices")
-      .where("private_key", privateKey)
+      .select( "question", "title" )
+      .from( "polls" )
+      .join( "choices", "polls.id", "choices.poll_id" )
+      .where( "private_key", privateKey )
       .then((results) => {
         if (results.length) {
           smsPoll += results[0].question;
           for( var i = 0; i < results.length; i++ ) {
-            smsPoll += "\n" + i + results[i].title;
+            smsPoll += "\n" + i+1 + " " + results[i].title;
           }
           if( smsPoll.length > 1600 ) {
             smsPoll = smsPoll.slice( 0, 1600 );
